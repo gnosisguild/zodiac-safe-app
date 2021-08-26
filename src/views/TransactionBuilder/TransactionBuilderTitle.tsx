de8559fe09cc58c2e0fc9f504b617b1351ccddb9
samepant <sampanter@gmail.com>
2021-08-26 11:16:38 -0400

    adjust size of helper text

--- src/views/TransactionBuilder/TransactionBuilderTitle.tsx
+++ src/views/TransactionBuilder/TransactionBuilderTitle.tsx
@@ -6,8 +6,8 @@ const useStyles = makeStyles((theme) => ({
   tooltip: {
     padding: theme.spacing(1, 0.5, 1, 1),
     backgroundColor: theme.palette.common.white,
-    maxWidth: 165,
-    fontSize: 10,
+    maxWidth: 250,
+    fontSize: 14,
     color: theme.palette.text.primary,
   },
   questionIcon: {
@@ -23,7 +23,7 @@ export const TransactionBuilderTitle = () => {
 
       <Tooltip
         classes={{ tooltip: classes.tooltip }}
-        title="Bundling transactions, allows you to setup multiple contract interactions and send them in one transaction, saving you gas"
+        title="Bundling transactions allows you to setup multiple contract interactions and send them in one transaction, saving you gas"
       >
         <QuestionMarkIcon className={classes.questionIcon} />
       </Tooltip>
