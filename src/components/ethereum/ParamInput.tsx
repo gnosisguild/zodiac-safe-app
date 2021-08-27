3053932e856e3f3dabee85053fe5dac438bf763d
Carlos <carlosfebres@dorg.tech>
2021-08-26 23:08:20 -0400

    feat: add time select for specific function

--- src/components/ethereum/ParamInput.tsx
+++ src/components/ethereum/ParamInput.tsx
@@ -3,11 +3,12 @@ import { ParamType } from "@ethersproject/abi";
 import { TextField, TextFieldProps } from "../input/TextField";
 import { formatParamValue } from "../../utils/contracts";
 import { MenuItem } from "@material-ui/core";
+import { BigNumber } from "ethers";
 
 export interface ParamInputProps
   extends Omit<TextFieldProps, "onChange" | "value" | "label"> {
   param: ParamType;
-  value?: string | boolean;
+  value?: string | boolean | BigNumber;
   label?: string;
 
   onChange(value: any, valid: boolean): void;
---
+++ src/views/ModuleDetails/transaction/TransactionField.tsx
@@ -0,0 +1,43 @@
+import {
+  ParamInput,
+  ParamInputProps,
+} from "../../../components/ethereum/ParamInput";
+import React from "react";
+import { FunctionFragment } from "@ethersproject/abi";
+import { TimeSelect } from "../../../components/input/TimeSelect";
+
+interface TransactionFieldProps {
+  param: ParamInputProps;
+  func: FunctionFragment;
+}
+
+export const TransactionField = ({ param, func }: TransactionFieldProps) => {
+  if (func.inputs.length === 1) {
+    const [input] = func.inputs;
+    const isTimeField = ["setTxCooldown", "setTxExpiration"].includes(
+      func.name
+    );
+    const isTime = isTimeField && input.type.includes("int");
+    if (isTime) return <TransactionTimeField {...param} />;
+  }
+
+  return <ParamInput {...param} />;
+};
+
+export const TransactionTimeField = ({
+  label,
+  value,
+  onChange,
+  ...props
+}: ParamInputProps) => {
+  const defaultValue = parseInt((value as string) || "0");
+  return (
+    <TimeSelect
+      {...props}
+      label={label || "Time"}
+      defaultValue={defaultValue}
+      defaultUnit="minutes"
+      onChange={(time) => onChange(time, true)}
+    />
+  );
+};
