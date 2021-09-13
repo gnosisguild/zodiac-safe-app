d675588eea1f2a539b15d785c1f3c28fceaeeee2
samepant <sampanter@gmail.com>
2021-09-13 14:39:34 -0400

    delay formatter tweak

--- src/utils/string.ts
+++ src/utils/string.ts
@@ -5,22 +5,22 @@ export function shortAddress(address: string) {
 export function formatDuration(duration: number) {
   if (duration < 60) {
     if (duration === 1) return `1 second`;
-    return `${duration} seconds`;
+    return `${duration} second`;
   }
 
   if (duration < 3600) {
     const minutes = Math.floor(duration / 60);
     if (minutes === 1) return `1 minute`;
-    return `${minutes} minutes`;
+    return `${minutes} minute`;
   }
 
   // Display until 47 hours
   if (duration < 172800) {
     const hours = Math.floor(duration / 3600);
     if (hours === 1) return `1 hour`;
-    return `${hours} hours`;
+    return `${hours} hour`;
   }
 
   const days = Math.floor(duration / 86400);
-  return `${days} days`;
+  return `${days} day`;
 }
