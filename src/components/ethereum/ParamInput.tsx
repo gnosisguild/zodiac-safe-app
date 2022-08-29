0a4c4905d31cf7d921f3b2315ec38f7f357f8472
Asgeir <asgeir@cluda.com>
2022-08-29 12:39:47 +0200

    Remove console warning regarding controlled component used as an uncontrolled component

--- src/components/ethereum/ParamInput.tsx
+++ src/components/ethereum/ParamInput.tsx
@@ -1,73 +1,65 @@
-import React, { useState } from "react";
-import { ParamType } from "@ethersproject/abi";
-import { ZodiacTextField } from "zodiac-ui-components";
-import { TextFieldProps } from "../input/TextField";
-import { formatParamValue } from "../../utils/contracts";
-import { MenuItem } from "@material-ui/core";
-import { BigNumber } from "ethers";
+import React, { useState } from "react"
+import { ParamType } from "@ethersproject/abi"
+import { ZodiacTextField } from "zodiac-ui-components"
+import { TextFieldProps } from "../input/TextField"
+import { formatParamValue } from "../../utils/contracts"
+import { MenuItem } from "@material-ui/core"
+import { BigNumber } from "ethers"
 
-export interface ParamInputProps
-  extends Omit<TextFieldProps, "onChange" | "value" | "label"> {
-  param: ParamType;
-  value?: string | boolean | BigNumber;
-  label?: string;
+export interface ParamInputProps extends Omit<TextFieldProps, "onChange" | "value" | "label"> {
+  param: ParamType
+  value?: string | boolean | BigNumber
+  label?: string
 
-  onChange(value: any, valid: boolean): void;
+  onChange(value: any, valid: boolean): void
 }
 
 function getLabel(param: ParamType) {
   if (param.name) {
-    return `${param.name} (${param.type})`;
+    return `${param.name} (${param.type})`
   }
-  return `(${param.type})`;
+  return `(${param.type})`
 }
 
-function getDefaultValue(
-  param: ParamType,
-  defaultValue: ParamInputProps["value"]
-): string {
+function getDefaultValue(param: ParamType, defaultValue: ParamInputProps["value"]): string {
   if (defaultValue !== undefined) {
-    if (typeof defaultValue === "object") return JSON.stringify(defaultValue);
-    return defaultValue.toString();
+    if (typeof defaultValue === "object") return JSON.stringify(defaultValue)
+    return defaultValue.toString()
   }
-  return param.baseType === "boolean" ? "false" : "";
+  return param.baseType === "boolean" ? "false" : ""
 }
 
-export const ParamInput = ({
-  param,
-  value: defaultValue,
-  onChange,
-  ...props
-}: ParamInputProps) => {
-  const [value, setValue] = useState<string>(
-    getDefaultValue(param, defaultValue)
-  );
-  const [error, setError] = useState<string>();
+export const ParamInput = ({ param, value: defaultValue, onChange, ...props }: ParamInputProps) => {
+  if (props.defaultValue != null) {
+    throw new Error("This is a controlled component, `defaultValue` should not be used. Use `value` instead.")
+  }
+  const [value, setValue] = useState<string>(getDefaultValue(param, defaultValue))
+  const [error, setError] = useState<string>()
 
   const handleChange = (evt: React.ChangeEvent<HTMLInputElement>) => {
-    const _value = evt.target.value;
-    setValue(_value);
+    const _value = evt.target.value
+    setValue(_value)
 
     if (param.baseType === "boolean") {
-      onChange(_value === "true", true);
-      return;
+      onChange(_value === "true", true)
+      return
     }
 
     if (!_value.length) {
-      onChange(_value, false);
-      setError(undefined);
-      return;
+      onChange(_value, false)
+      setError(undefined)
+      return
     }
 
     try {
-      const paramValue = formatParamValue(param, _value);
-      onChange(paramValue, true);
-      setError(undefined);
+      const paramValue = formatParamValue(param, _value)
+      onChange(paramValue, true)
+      setError(undefined)
     } catch (error) {
-      onChange(_value, false);
-      setError(error.message);
+      onChange(_value, false)
+      setError(error.message)
     }
-  };
+  }
 
   if (param.baseType === "boolean") {
     return (
@@ -82,7 +74,7 @@ export const ParamInput = ({
         <MenuItem value="true">True</MenuItem>
         <MenuItem value="false">True</MenuItem>
       </ZodiacTextField>
-    );
+    )
   }
 
   return (
@@ -95,5 +87,5 @@ export const ParamInput = ({
       helperText={error}
       {...props}
     />
-  );
-};
+  )
+}
