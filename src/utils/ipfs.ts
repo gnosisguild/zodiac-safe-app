e749221bb3d686faab8ce44297b7f1941a12fcfd
juliopavila <javila1997@gmail.com>
2022-09-16 22:49:44 +0200

    add ipfs-core as cdn

--- src/utils/ipfs.ts
+++ src/utils/ipfs.ts
@@ -1,33 +1,35 @@
-import * as IPFS from "ipfs-core";
+// import * as IPFS from "ipfs-core"
 
-let node: IPFS.IPFS;
-const textDecoder = new TextDecoder();
+let node: any = null
+const textDecoder = new TextDecoder()
 
 const getNode = async () => {
   if (node == null) {
-    node = await IPFS.create();
+    // node = await IPFS.create()
+    //@ts-ignore
+    node = await window.IpfsCore.create()
   }
-  return node;
-};
+  return node
+}
 export const getJsonData = async (path: string) => {
-  const node = await getNode();
+  const node = await getNode()
 
   if (path.startsWith("ipns")) {
-    path = await node.resolve(path, { recursive: true });
+    path = await node.resolve(path, { recursive: true })
   }
 
-  path = path.replace("ipfs/", "");
+  path = path.replace("ipfs/", "")
 
-  const chunks: string[] = [];
+  const chunks: string[] = []
   for await (const chunk of node.cat(path)) {
-    chunks.push(textDecoder.decode(chunk));
+    chunks.push(textDecoder.decode(chunk))
   }
-  const rawContent = chunks.join("");
-  return JSON.parse(rawContent);
-};
+  const rawContent = chunks.join("")
+  return JSON.parse(rawContent)
+}
 
 export const add = async (content: any) => {
-  const node = await getNode();
-  const { cid } = await node.add(content);
-  return cid;
-};
+  const node = await getNode()
+  const { cid } = await node.add(content)
+  return cid
+}
