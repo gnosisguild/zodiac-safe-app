937afdaed7d1e4989ef60441019e3bb9e734455e
Asgeir <asgeir@cluda.com>
2022-09-29 11:34:22 +0200

    Init Wrangler version
    With environment problems. The wrangler environment is not a real Node.js environment

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,15 +1,41 @@
-import { Env } from "./types"
+import { Body, NotificationType } from "./types"
+import { SentinelClient } from "defender-sentinel-client"
 
-export default {
-  async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-    const { pathname } = new URL(request.url)
+addEventListener("fetch", async (event) => {
+  const { request, respondWith } = event
+  const { pathname } = new URL(request.url)
 
-    // Recreate the response so you can modify the headers
-    const response = new Response("Hello World!")
+  if (request.method === "POST" && pathname.startsWith("/monitoring/notification")) {
+    try {
+      const text = await request.text()
+      const body = JSON.parse(text) as Body
+      const { apiKey, apiSecret, channel, config } = body
+      const client = new SentinelClient({ apiKey, apiSecret })
+      console.log("client is set up")
 
-    // Set CORS headers
-    response.headers.set("Access-Control-Allow-Origin", "*")
+      const notificationSetupResponds = await setupNewNotification(client, channel, config)
+      console.log(notificationSetupResponds)
+      // Recreate the response so you can modify the headers
+      const notificationSetupRespondsJson = JSON.stringify(notificationSetupResponds, null, 2)
+      const response = new Response(notificationSetupRespondsJson, {
+        headers: {
+          "content-type": "application/json;charset=UTF-8",
+          "Access-Control-Allow-Origin": "*",
+        },
+      })
+      return respondWith(response)
+    } catch (e) {
+      console.error(e)
+      return respondWith(new Response("error", { status: 500 }))
+    }
+  }
+  return respondWith(new Response("error", { status: 500 }))
+})
 
-    return response
-  },
-}
+const setupNewNotification = (client: SentinelClient, channel: NotificationType, config: any) =>
+  client.createNotificationChannel({
+    type: channel,
+    name: `ZodiacRealityModuleNotification-${channel}`,
+    config,
+    paused: false,
+  })
--- packages/backend/src/types.ts
+++ packages/backend/src/types.ts
@@ -1,1 +1,9 @@
-export interface Env {}
+import { NotificationType } from "defender-sentinel-client"
+export type { NotificationType }
+
+export interface Body {
+  apiKey: string
+  apiSecret: string
+  channel: NotificationType
+  config: any
+}
