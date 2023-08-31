a58bfd8e776125ff36e7b032de44d50f1975aa60
auryn-macmillan <auryn-macmillan@users.noreply.github.com>
2023-08-31 16:16:02 +0000

    Deploying to gh-pages from @ gnosis/zodiac-safe-app@5b2c91fc818b60d49bc19b61956de9dc0d8ab70c 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.169aa23dfa053f2295d34db69c82af67.js"
+  "./precache-manifest.065c72c849463a5793954e54e05d07be.js"
 );
 
 self.addEventListener('message', (event) => {
